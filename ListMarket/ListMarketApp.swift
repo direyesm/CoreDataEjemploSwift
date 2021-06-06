4881c3eccaafd0b91fe3fb7e7e28aedd715ce812
direyesm <37608963+direyesm@users.noreply.github.com>
2021-06-06 09:17:24 -0400

    Initial Commit

---
+++ ListMarket/ListMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListMarketApp.swift
+//  ListMarket
+//
+//  Created by Diego on 06-06-21.
+//
+
+import SwiftUI
+
+@main
+struct ListMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
